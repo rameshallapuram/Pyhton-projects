d8e334cd63971dd56ed0d2ba128337b8d5a5c12c
Ramesh <rameshallapuram@gmail.com>
2022-07-04 11:23:49 +0530

    Blackjack game

---
+++ Black Jack/BlackJack.py
@@ -0,0 +1,99 @@
+from os import system,name
+import random
+import art
+print("Welcome to the game of BlackJack!!")
+print(art.logo)
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def deal():
+    """ Returns a random card """
+    card_list = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(card_list)
+    return card
+
+def calculate_score(cards):
+    sum(cards)
+    if sum(cards) == 21 and len(cards) == 2:
+        return 21
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards) 
+
+
+def compare(user_score, dealer_score):
+    if user_score > 21 and dealer_score > 21:
+        return "You went over. You lose"
+    elif user_score == dealer_score:
+        return "Draw"
+    elif dealer_score == 21:
+        return " You loose, opponent has Blackjack"
+    elif user_score == 21:
+        return "Win with a Blackjac "
+    elif user_score > 21:
+        return "You went over. You loose"
+    elif dealer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > dealer_score:
+        return "You win"
+    else:
+        return "You loose"
+
+
+def game():
+    game_over = False
+    user_hand = []
+    dealer_hand = []
+    
+    for i in range(2):
+        user_hand.append(deal())
+        dealer_hand.append(deal())
+
+    while not game_over:
+        user_score = calculate_score(user_hand)
+        dealer_score = calculate_score(dealer_hand)
+        print(f"user hand is {user_hand} and the score is {user_score}.")
+        print(f"Dealer's first hand is {dealer_hand[0]}.")
+
+        if user_score == 21 or dealer_score == 21 or user_score > 21:
+            game_over = True
+        else:
+            user_choice = input("Do you want to pick a card or stand? 'p' or 's': ").lower()
+            if user_choice == "s":
+                game_over = True
+            else:
+                user_hand.append(deal())
+                user_score = calculate_score(user_hand)
+    
+    while dealer_score != 21 or dealer_score < 17:
+        dealer_hand.append(deal())
+        dealer_score = calculate_score(dealer_hand)
+
+    print(f"Your final hand is {user_hand} and final score is {user_score}.")
+    print(f"Dealer's final hand is {dealer_hand} and final score is {dealer_score}.")
+    print(compare(user_score, dealer_score))
+
+while input("Do you want to play? 'yes' or 'no'") == "yes":
+    clear()
+    game()
+else:
+    game_over = True
+    print("Thank you playing. Bye!")
+
+
+
+    
+
+
+
+
+
+
+
+        
